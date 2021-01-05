09675512ce34bcec0c3fb71d975479ee52c9e18f
Stas <stanislav.parfilov@ics-it.ru>
2021-01-06 01:34:54 +0300

    test

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
 from flask import Flask
+import os
+import psycopg2
 app = Flask(__name__)
  
  
 @app.route('/')
 def hello():
-    return "Hello World!"
+    DATABASE_URL = os.environ['postgresql-rectangular-84724']
+    conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM random_user')
+    records = cursor.fetchall()
+    
+    return records
  
 if __name__ == '__main__':
     app.run()
