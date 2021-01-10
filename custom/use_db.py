c3874dcbb1e3bc1cfb4181c685313abeee0765d8
Stas <stanislav.parfilov@ics-it.ru>
2021-01-10 19:24:25 +0300

    'sql'

--- custom/use_db.py
+++ custom/use_db.py
@@ -1,11 +1,11 @@
 import psycopg2
+from psycopg2 import sql
 import os
-
 
 class UseDB():
     def __init__(self):
         pass
-
+        
     def connect_db(self):
         DATABASE_URL = os.environ['DATABASE_URL']
         conn = psycopg2.connect(DATABASE_URL, sslmode='require')
@@ -13,23 +13,21 @@ class UseDB():
         return conn, cursor
 
     def all_users(self):
-        sql = "select Name from random_user"
+        sql_string = 'select Name from random_user'
         users = []
         conn, cursor = self.connect_db()
-        cursor.execute(sql)
+        cursor.execute(sql_string)
         records = cursor.fetchall()
         for user in records:
             users.append(user[0])
         return users
 
     def insert_user(self, insert_username):
-        sql = "insert into random_user (Name) values(%s')"
         conn, cursor = self.connect_db()
-        cursor.execute(sql, (insert_username, ))
+        cursor.execute(sql.SQL("insert into random_user (Name) values(%s)"), [insert_username,])
         conn.commit()
 
     def delete_user(self, delete_username):
-        sql = "delete from random_user where Name =%s'"
         conn, cursor = self.connect_db()
-        cursor.execute(sql, (delete_username, ))
+        cursor.execute(sql.SQL("delete from random_user where Name = %s"), [delete_username,])
         conn.commit()
